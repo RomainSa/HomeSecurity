f8dc54a36374c1b1a9d690b8cb4704b13a9394ef
RomainSa <romain.savidan@gmail.com>
2017-05-26 18:35:46 +0200

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 PHOTO_NB=1
+START_DATE=$(date +"%d%m%Y")
 
-while true
-  do
-  DATE=$(date +"%u")
-  if [ $PHOTO_NB = 1 ]
-  then
-    rm snap_"$DATE"*.jpg
-  fi
-  raspistill --width 1280 --height 960 --quality 100 -o /home/pi/camera/snap_"$DATE"_"$PHOTO_NB".jpg
-  /home/pi/Dropbox-Uploader/dropbox_uploader.sh upload /home/pi/camera/snap_"$DATE"_"$PHOTO_NB".jpg /
-  ((PHOTO_NB++))
+while [ "$START_DATE" == "$(date +"%d%m%Y")" ]
+    do
+    DATE=$(date +"%u")
+    if [ $PHOTO_NB = 1 ]
+    then
+        rm snap_"$DATE"*.jpg
+    fi
+    raspistill --width 1280 --height 960 --quality 100 -o /home/pi/camera/snap_"$DATE"_"$PHOTO_NB".jpg
+    /home/pi/Dropbox-Uploader/dropbox_uploader.sh upload /home/pi/camera/snap_"$DATE"_"$PHOTO_NB".jpg /
+    ((PHOTO_NB++))
 done
-
